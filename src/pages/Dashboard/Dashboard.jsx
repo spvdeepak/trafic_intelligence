d870134ae2ce1ba352263ffbd63238dc65a5cbfb
Deepak Kumar <meetspvdeepak@gmail.com>
2024-04-25 13:52:31 +0530

    dash update

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'leaflet';
 
 const Dashboard = () => {
     const [userLatitude, setUserLatitude] = useState(null);
     const [userLongitude, setUserLongitude] = useState(null);
-    // const [destinationLatitude, setDestinationLatitude] = useState(null);
-    // const [destinationLongitude, setDestinationLongitude] = useState(null);
-    // const [map, setMap] = useState(null);
 
     useEffect(() => {
         if (navigator.geolocation) {
@@ -19,39 +15,38 @@ const Dashboard = () => {
         } else {
             console.warn('Geolocation is not supported by this browser.');
         }
-
-        // const initialPosition = [userLatitude || 51.505, -0.09]; // Default to London if no geolocation
-        // const leafletMap = new L.map('map').setView(initialPosition, 13);
-        // setMap(leafletMap);
-
-        // const attribution =
-        //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-        // const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        // const tiles = new L.tileLayer(tileUrl, { attribution });
-        // tiles.addTo(leafletMap);
-
-        // // Handle user click on map to set destination
-        // leafletMap.on('click', (e) => {
-        //     const { lat, lng } = e.latlng;
-        //     setDestinationLatitude(lat);
-        //     setDestinationLongitude(lng);
-        // });
     }, []);
 
     return (
-        <div>
-            {/* <MapContainer center={[userLatitude || 51.505, -0.09]} zoom={13} whenCreated={setMap} style={{ height: '300px' }} id="map">
-                <TileLayer attribution="" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                {userLatitude && userLongitude && (
-                    <Marker position={[userLatitude, userLongitude]}>
-                        <Popup>Your Location</Popup>
-                    </Marker>
-                )}
-            </MapContainer> */}
-            <input type="text" name="latitude" value={userLatitude} />
-            <input type="text" name="longitude" value={userLongitude} />
-            {/* <input type="text" name="destination_latitude" value={destinationLatitude} />
-            <input type="text" name="destination_longitude" value={destinationLongitude} /> */}
+        <div className='flex-row justify-center items-center h-screen font-primary bg-[#00ffff] p-4'>
+
+            {/* <div className="absolute top-0 text-3xl font-title mt-24 font-semibold uppercase">Traffic Intelligence</div> */}
+            
+            <h1 className="text-2xl font-semibold mb-4 font-title text-center">Current Coordinates</h1>
+
+            <div className="mb-4">
+                <label className="uppercase block text-gray-600 text-xl pb-3">Lattitude</label>
+                <input type="text" name="latitude" value={userLatitude} disabled className='w-full border border-gray-300 rounded-lg py-5 px-4 text-2xl focus:outline-none focus:border-blue-500 bg-white'/>
+            </div>
+            <div className="mb-4">
+                <label className="uppercase block text-gray-600 text-xl pb-3">Lattitude</label>
+                <input type="text" name="longitude" value={userLongitude} disabled className='w-full border border-gray-300 rounded-lg py-5 px-4 text-2xl focus:outline-none focus:border-blue-500 bg-white'/>
+            </div>
+
+            <h1 className="text-2xl font-semibold mb-4 font-title text-center">Enter Destination coordinates</h1>
+
+            <div className="mb-4">
+                <label className="uppercase block text-gray-600 text-xl pb-3">Lattitude</label>
+                <input type="text" name="destination_latitude" className="w-full border border-gray-300 rounded-lg py-5 px-4 text-2xl focus:outline-none focus:border-blue-500" autocomplete="off" required />
+            </div>
+
+            <div className="mb-4">
+                <label className="uppercase block text-gray-600 text-xl pb-3">Longitude</label>
+                <input type="text" name="destination_latitude" className="w-full border border-gray-300 rounded-lg py-5 px-4 text-2xl focus:outline-none focus:border-blue-500" autocomplete="off" required />
+            </div>
+
+            <button type="submit" className="bg-blue-500 font-title tracking-wider uppercase hover:bg-blue-600 text-white font-semibold rounded-md py-4 px-4 left-0 right-0 absolute bottom-8 w-[90%] m-auto">Start Emergency Trip</button>
+
         </div>
     );
 }
